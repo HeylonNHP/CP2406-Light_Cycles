ce1e5cef1d538e842fdfae4a66b3f27ba213856c
Heylon <Heylon96@hotmail.com>
2017-08-29 17:09:47 +1000

    Basic Server UI designed

--- Server/src/com/company/Main.java
+++ Server/src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class Main {
     public static JFrame mainWindow;
+    public static GridLayout mainWindowGridLayout;
     public static void main(String[] args) {
         // write your code here
         mainWindow = new JFrame();
@@ -12,10 +13,30 @@ public class Main {
         mainWindow.pack();
 
         Dimension windowSize = new Dimension();
-        windowSize.width = 500;
-        windowSize.height = 500;
+        windowSize.width = 300;
+        windowSize.height = 200;
 
         mainWindow.setSize(windowSize);
+
+        mainWindowGridLayout = new GridLayout(0,2);
+        mainWindow.setLayout(mainWindowGridLayout);
+
+        JLabel widthLabel = new JLabel("Grid width");
+        JTextField widthTextInput = new JTextField();
+        widthTextInput.setText("500");
+        JLabel heightLabel = new JLabel("Grid height");
+        JTextField heightTextInput = new JTextField();
+        heightTextInput.setText("500");
+
+        mainWindow.add(widthLabel);
+        mainWindow.add(widthTextInput);
+        mainWindow.add(heightLabel);
+        mainWindow.add(heightTextInput);
+
+        JButton startGameButton = new JButton("Start game!");
+        mainWindow.add(startGameButton);
+
+
         mainWindow.setVisible(true);
     }
 }
